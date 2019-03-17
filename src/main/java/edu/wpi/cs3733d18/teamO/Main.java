f3fa550ad568d79f3256306a5d55e7de45633129
Dev <devrc13@gmail.com>
2019-03-17 15:13:45 -0400

    Merge branch 'master' of https://github.com/cs3733-d19-team-o/CS3733-D19-Team-O-GitExercise
    # Conflicts:
    #	src/main/java/edu/wpi/cs3733d18/teamO/Main.java

--- src/main/java/edu/wpi/cs3733d18/teamO/Main.java
+++ src/main/java/edu/wpi/cs3733d18/teamO/Main.java
@@ -2,26 +2,27 @@ package edu.wpi.cs3733d18.teamO;
 
 public final class Main {
 
-  private Main() {
+    private Main() {
 
-  }
+    }
 
-  public static void main(String... args) {
-      System.out.println("Austin");
-      System.out.println("Ken");    
-      System.out.println("Hello, World");
-      System.out.println("Dev Patel");
-      System.out.println("Jyan");
-      System.out.println("Amelia");
-      System.out.println("Sam");
-      System.out.println("Peter Prygocki");
-      System.out.println("Jack Yang");
-      System.out.println("Lynn Song");
-      System.out.println("DevBranch");
-      printDev();
-  }
+    public static void main(String... args) {
+        System.out.println("Austin");
+        System.out.println("Ken");
+        System.out.println("Hello, World");
+        System.out.println("Dev Patel");
+        System.out.println("Jyan");
+        System.out.println("Amelia");
+        System.out.println("Sam");
+        System.out.println("Peter Prygocki");
+        System.out.println("Jack Yang");
+        System.out.println("Lynn Song");
+        System.out.println("DevBranch");
+        printDev();
+    }
 
-  private static void printDev() {
-      System.out.println("Dev Branch");
-  }
+    private static void printDev() {
+        System.out.println("BranchDev");
+    }
+
 }
