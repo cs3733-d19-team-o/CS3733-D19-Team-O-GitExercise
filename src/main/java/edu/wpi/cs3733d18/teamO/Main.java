2e130b2611af336a367c3cf960faef0633fafae4
Lynn <jsong@wpi.edu>
2019-03-17 14:57:44 -0400

    add new branch to test

--- src/main/java/edu/wpi/cs3733d18/teamO/Main.java
+++ src/main/java/edu/wpi/cs3733d18/teamO/Main.java
@@ -7,15 +7,6 @@ public final class Main {
   }
 
   public static void main(String... args) {
-      System.out.println("Austin");
-      System.out.println("Ken");
-      System.out.println("Hello, World");
-      System.out.println("Dev Patel");
-      System.out.println("Jyan");
-      System.out.println("Amelia");
-      System.out.println("Sam");
-      System.out.println("Peter Prygocki");
-      System.out.println("Jack Yang");
-      System.out.println("Lynn Song");
+      System.out.println("Branch-lynn");
   }
 }
